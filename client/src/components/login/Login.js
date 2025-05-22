411f7bda7a411af5375a9c8af3e6908c2cdab203
Anmol0127 <124490062+Anmol0127@users.noreply.github.com>
2025-05-22 05:54:55 +0530

    Update Login.js

--- client/src/components/login/Login.js
+++ client/src/components/login/Login.js
@@ -12,11 +12,11 @@ const Login = () => {
       }}>
       <div className="flex flex-col items-center mt-10 space-y-32">
         <h1 className="text-3xl font-semibold bg-black text-white w-full text-center py-4 bg-opacity-75 rounded-2xl">
-          CMS - Course Management System
+          EduTrack - A modern CMS 💻
         </h1>
         <div className="grid grid-cols-2 gap-28">
           <div className="h-96 w-96 space-y-11 shadow-2xl flex flex-col justify-center items-center bg-transparent backdrop-blur-md bg-[#5a51d6] bg-opacity-60 rounded-xl">
-            <h1 className="text-4xl font-extrabold">Faculty</h1>
+            <h1 className="text-4xl font-extrabold">Faculty 👩‍🏫</h1>
 
             <Link
               type="button"
@@ -26,7 +26,7 @@ const Login = () => {
             </Link>
           </div>
           <div className="h-96 w-96 space-y-11 shadow-2xl flex flex-col justify-center items-center bg-transparent backdrop-blur-md bg-[#d65158] bg-opacity-60 rounded-xl">
-            <h1 className="text-4xl font-extrabold">Student</h1>
+            <h1 className="text-4xl font-extrabold">Student 👩‍🎓</h1>
             <Link
               type="button"
               to="/login/studentlogin"
